a974c3e6048c78f04019be780f031d24135f05bf
Ian Hu <hu2008yinxiang@163.com>
2016-08-09 23:09:15 +0800

    panel

--- src/js/components/what-to-eat/page/Home.js
+++ src/js/components/what-to-eat/page/Home.js
@@ -2,7 +2,7 @@
  * @Author: Ian Hu
  * @Date:   2016-08-03 21:36:16
  * @Last Modified by:   Ian Hu
- * @Last Modified time: 2016-08-03 23:37:49
+ * @Last Modified time: 2016-08-09 23:04:34
  */
 
 'use strict';
@@ -28,11 +28,59 @@ export class Home extends React.Component {
     render() {
 
         return (
-            <Jumbotron>
-                <h1>要吃啥</h1>
-                <p>每天要吃啥不再是你头疼的问题</p>
-                <p><a href="#">马上解决</a></p>
-            </Jumbotron>
+            <div>
+                <Jumbotron style={{marginBottom: '4px'}}>
+                    <div className="container">
+                        <h1>要吃啥</h1>
+                        <p>每天要吃啥不再是你头疼的问题</p>
+                        <p><a href="#">马上解决</a></p>
+                    </div>
+                </Jumbotron>
+            <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div className="panel panel-default">
+    <div className="panel-heading" role="tab" id="headingOne">
+      <h4 className="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          最常纠结
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div className="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div className="panel panel-default">
+    <div className="panel-heading" role="tab" id="headingTwo">
+      <h4 className="panel-title">
+        <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          更多纠结
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div className="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div className="panel panel-default">
+    <div className="panel-heading" role="tab" id="headingThree">
+      <h4 className="panel-title">
+        <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          设置
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div className="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+            </div>
         );
     }
 }
